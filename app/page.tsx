5473b40d5f22660641aed229258e14a33e3c585f
akariNguyen <hoangphumau123@gmail.com>
2026-06-20 21:45:22 +0700

    ok4

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/header1";
 import Footer from "@/components/footer";
 import TrangChu from "./trangchu/trangchu";
 import FloatingContact from "@/components/floating-contact";
